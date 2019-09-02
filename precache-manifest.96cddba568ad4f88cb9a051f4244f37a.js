33140b98eb6872274fcdc878d4cf6ed958b59711
ramiasg <rami.sweyri@gmail.com>
2019-09-02 21:44:57 +0300

    Updates

---
+++ precache-manifest.96cddba568ad4f88cb9a051f4244f37a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b2a2b1f1319f5728125d8cda33e74f",
+    "url": "/accountant-app/index.html"
+  },
+  {
+    "revision": "85d6cdf770fe893cafb1",
+    "url": "/accountant-app/static/css/main.4eb9a46f.chunk.css"
+  },
+  {
+    "revision": "8d1184c970ed2a94dfa3",
+    "url": "/accountant-app/static/js/2.15d111fa.chunk.js"
+  },
+  {
+    "revision": "85d6cdf770fe893cafb1",
+    "url": "/accountant-app/static/js/main.96dc3ea2.chunk.js"
+  },
+  {
+    "revision": "7eac35a65df94af242a0",
+    "url": "/accountant-app/static/js/runtime~main.490fe0ef.js"
+  },
+  {
+    "revision": "af74b5506aa9ffc26511f5cb803d5fe1",
+    "url": "/accountant-app/static/media/companies.af74b550.svg"
+  },
+  {
+    "revision": "4f90b1fe0306d3e7520b44150695f686",
+    "url": "/accountant-app/static/media/exp.4f90b1fe.svg"
+  },
+  {
+    "revision": "6369c7175ace050a43c32ed3cbd25e53",
+    "url": "/accountant-app/static/media/home.6369c717.svg"
+  },
+  {
+    "revision": "aef17a6f75f8007998200e9d175869cb",
+    "url": "/accountant-app/static/media/inc.aef17a6f.svg"
+  },
+  {
+    "revision": "d843562e57d02cef2a2a502c555e9c40",
+    "url": "/accountant-app/static/media/logo.d843562e.svg"
+  }
+]);
